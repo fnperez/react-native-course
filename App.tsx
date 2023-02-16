dd283a9ae5f1f140798ff18b70b7c6bdbdacae2f
Matias <matiaszanan@gmail.com>
2023-02-16 17:26:47 -0300

    delete text logo & create button component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { View } from 'react-native'
+import Button from './src/components/DesignSystem/Button/Button';
 import Logo from './src/components/DesignSystem/Logo/Logo'
 
 const App = () => (
-    <View style={{ backgroundColor: '#FFF' }}>
-        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-            <Logo size='sm'/>
-            <Logo size='md'/>
-            <Logo size='lg'/>
-            <Logo size='lg' type='stacked'/>
-        </View>
+  <View style={{ backgroundColor: '#1D1D1D', flex: 1 }}>
+    <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+      <Logo size='sm'/>
+      <Logo size='md'/>
+      <Logo size='lg'/>
     </View>
+    <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+      <Button
+          onPress={() => console.log('Button Filled Pressed')} 
+          icon='android'
+          type='filled' 
+          title='Filled btn'
+      />
+      <Button 
+          onPress={() => console.log('Button Outline Pressed')} 
+          type='outline' 
+          title='Outline btn'
+      />
+      <Button 
+          onPress={() => console.log('Button Link Pressed')} 
+          type='link' 
+          title='Link btn'
+      />
+    </View>
+  </View>
 )
 
 export default App;
---
+++ src/components/DesignSystem/Button/Style.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+export const DefaultStyle = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  title: {
+    color: '#FFFFFF'
+  },
+  icon: {
+    color: '#FFFFFF'
+  }
+})
+
+export const FilledStyle = (active: boolean) => StyleSheet.create({
+  container: {
+    backgroundColor: active ? '#8687E7AF' : '#8687E7',
+    borderRadius: 4
+  }
+})
+
+export const OutlineStyle = (active: boolean) => StyleSheet.create({
+  container: {
+    backgroundColor: '#transparent',
+    borderColor: active ? '#8687E7AF' : '#8687E7',
+    borderRadius: 4,
+    borderWidth: 2
+  }
+})
+
+export const LinkStyle = (active: boolean) => StyleSheet.create({
+  container: {
+    backgroundColor: '#transparent',
+    borderWidth: 0
+  },
+  title: {
+    color: active ? '#FFFFFFA0' : '#FFFFFF'
+  }
+})
